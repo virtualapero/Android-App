12788e2b560abac8d2eff45fa75c3abd11ddc3de
Quentin <quentin-schuster@web.de>
2021-09-23 20:47:00 +0200

    update theme

--- src/main.dart
+++ src/main.dart
@@ -38,18 +38,21 @@ class MyApp extends StatelessWidget {
         primarySwatch: const MaterialColor(
             0xFF007cba,
             <int, Color>{
-              50: Color(0xFFE0EFF7),
-              100: Color(0xFFB3D8EA),
-              200: Color(0xFF80BEDD),
-              300: Color(0xFF4DA3CF),
-              400: Color(0xFF2690C4),
-              500: Color(0xFF007CBA),
-              600: Color(0xFF0074B3),
-              700: Color(0xFF0069AB),
-              800: Color(0xFF005FA3),
-              900: Color(0xFF004C94),
+              50: Color(0xFF0070a7),
+              100: Color(0xFF006395),
+              200: Color(0xFF005782),
+              300: Color(0xFF004a70),
+              400: Color(0xFF003e5d),
+              500: Color(0xFF00324a),
+              600: Color(0xFF002538),
+              700: Color(0xFF001925),
+              800: Color(0xFF000c13),
+              900: Color(0xFF000000),
             }
         ),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+            selectedItemColor: Color(0xFF007cba)
+        )
       ),
       themeMode: ThemeMode.system,
       onGenerateRoute: AppRouter.generateRoute,
