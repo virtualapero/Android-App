4c7be4a6a4e7ad8489e309496e3f60198785ad38
Quentin <quentin-schuster@web.de>
2021-09-23 20:29:18 +0200

    add content to first time view

--- src/pages/first_time.dart
+++ src/pages/first_time.dart
@@ -8,16 +8,50 @@ class FirstTimeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: GestureDetector(
-          child: const Text('first'),
-          onTap: () => {
-            AppRouter.first(),
-            Navigator.of(context).pushNamed(AppRouter.mainRoute)
-          },
-        ),
-      ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                SizedBox(
+                  width: double.infinity,
+                  child: Text(
+                    "VirtualApéro",
+                    style:
+                    TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(
+                  height: 52.0,
+                ),
+                Text("Placeholder Text"),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: MaterialButton(
+                color: Theme.of(context).primaryColor,
+                onPressed: () =>
+                {AppRouter.first(), Navigator.of(context).pushNamed("/")},
+                child: Text(
+                  "Weiter",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Theme.of(context).colorScheme.onPrimary
+                  ),
+                ),
+              ),
+            )
+          )
+        ],
+      )),
     );
   }
-
 }
