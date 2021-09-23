314e53c83502bfe895ac1180090065538eb45e72
Quentin <quentin-schuster@web.de>
2021-09-23 18:44:25 +0200

    change name to VirtualApero

--- src/main.dart
+++ src/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'VirtualApero',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
