33e6d59fcd2ed88ebee1633807dbdbbc11550eed
Quentin <quentin-schuster@web.de>
2021-09-23 21:16:20 +0200

    add bottom navigation to home view

--- src/pages/index.dart
+++ src/pages/index.dart
@@ -1,20 +1,64 @@
 import 'package:flutter/material.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeView();
+}
+
+class _HomeView extends State<HomeView> {
+
+  final List<BottomNavigationBarItem> bottomItems = [
+    BottomNavigationBarItem(
+        icon: Icon(Icons.wine_bar),
+        label: "Apéros"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.link),
+        label: "Links"
+    ),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.apps),
+        label: "Projekte"
+    )
+  ];
+  final List<Widget> _widgetOptions = <Widget>[
+    const Text(
+      'Apéros',
+    ),
+    const Text(
+      'Links',
+    ),
+    const Text(
+      'Projekte',
+    ),
+  ];
+
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: GestureDetector(
-          child: const Text('Home'),
+          child: _widgetOptions.elementAt(_selectedIndex),
           onTap: () => {
             Navigator.of(context).pushNamed('asd')
           },
         ),
       ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: bottomItems,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
     );
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 }
